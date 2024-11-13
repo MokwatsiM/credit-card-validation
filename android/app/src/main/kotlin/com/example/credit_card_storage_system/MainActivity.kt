54fefde1da2124ecbaea1df0e797e5ce3415bbb7
MokwatsiM <motebangmokwatsi0@gmail.com>
2024-11-13 22:05:24 +0200

    task: initial commit with all default files

---
+++ android/app/src/main/kotlin/com/example/credit_card_storage_system/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.credit_card_storage_system
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import apple_vision_recognize_text
+import path_provider_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppleVisionRecognizeTextPlugin.register(with: registry.registrar(forPlugin: "AppleVisionRecognizeTextPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
