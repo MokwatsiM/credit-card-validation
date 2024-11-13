981e826f65bf5860564e0de41d0fefe9a09cfcd9
MokwatsiM <motebangmokwatsi0@gmail.com>
2024-11-13 22:10:19 +0200

    task: main file initialise HIVE by getting application directory and create a storage path for hive inside creditCards path

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+
+import 'package:credit_card_storage_system/features/credit_card_scanner.dart';
+import 'package:credit_card_storage_system/models/credit_card_model.dart';
+import 'package:credit_card_storage_system/features/capture_card_details/views/credit_card_screen.dart';
+import 'package:credit_card_storage_system/features/list_card_details/views/credit_cards_list_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDir = await getApplicationDocumentsDirectory();
+
+
+  final hiveStoragePath = Directory('${appDocumentDir.path}/hive_data');
+
+  // Initialize Hive with a custom path
+  if (!Hive.isAdapterRegistered(1)) {
+    Hive.registerAdapter(CreditCardDataModelAdapter());
+  }
+
+  if (!Hive.isBoxOpen('creditCards')) {
+    Hive.init(hiveStoragePath.path);
+    await Hive.openBox<CreditCardDataModel>('creditCards');
+  }
+
+  // if (!Hive.isBoxOpen('creditCards')) {
+  //   Hive.init(hiveStoragePath.path);
+  //   Hive.registerAdapter(CreditCardModelAdapter());
+  //   await Hive.openBox<CreditCardModel>('creditCards');
+  // }
+
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Credit Card Capture App',
+      // theme: ThemeData(
+      //   primaryColor: AppColors.colorE5D1B2,
+      // ),
+      home: CreditCardScreen(),
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/':
+            return MaterialPageRoute(
+              builder: (context) => CreditCardScreen(),
+            );
+          case '/list':
+            return MaterialPageRoute(
+              builder: (context) => const CreditCardsListScreen(),
+            );
+          case '/scanner':
+            return MaterialPageRoute(
+                builder: (context) => const CreditCardScanner(),
+                settings: settings);
+          default:
+            throw Exception('Invalid route: ${settings.name}');
+        }
+      },
+    );
+  }
+}
