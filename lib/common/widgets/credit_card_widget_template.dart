c50a1e09bcb68907522d062aa8aaa73fe47988da
MokwatsiM <motebangmokwatsi0@gmail.com>
2024-11-15 01:20:07 +0200

    feature: use bloc for state management on card template

--- lib/common/widgets/credit_card_widget_template.dart
+++ lib/common/widgets/credit_card_widget_template.dart
@@ -1,41 +1,46 @@
 import 'package:credit_card_storage_system/common/widgets/card_decorated_box.dart';
 import 'package:credit_card_storage_system/models/credit_card_model.dart';
-import 'package:credit_card_storage_system/viewmodel/credit_card_notifier_viewmodel.dart';
+import 'package:credit_card_storage_system/blocs/credit_card_bloc.dart';
+import 'package:credit_card_storage_system/blocs/credit_card_event.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CreditCardWidgetTemplate extends ConsumerWidget {
+class CreditCardWidgetTemplate extends StatelessWidget {
   const CreditCardWidgetTemplate({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final CreditCardDataModel state;
-    if (super.key != null) {
-      state = ref
-          .watch(creditCardStateProvider.notifier)
-          .getAllCreditCards()
-          .where((element) => element.cardNumber == (key as ValueKey).value)
-          .first;
-    } else {
-      state = ref.watch(creditCardStateProvider);
-    }
-
-    return GestureDetector(
-        onTap: () =>
-            ref.read(creditCardStateProvider.notifier).toggleShowBackView(),
-        child: Stack(children: <Widget>[
-          AnimatedPositioned(
-              curve: Curves.elasticOut,
-              duration: const Duration(milliseconds: 2800),
-              child: AnimatedContainer(
-                  alignment: Alignment.center,
-                  duration: const Duration(milliseconds: 300),
-                  child: state.showBackView
-                      ? _buildBackView(cardModel: state, context: context)
-                      : _buildFrontView(cardModel: state, context: context)))
-        ]));
+  Widget build(BuildContext context) {
+    return BlocBuilder<CreditCardBloc, CreditCardDataModel>(
+      builder: (context, state) {
+        if (super.key != null) {
+          state = context
+              .watch<CreditCardBloc>()
+              .getAllCreditCards()
+              .where((element) => element.cardNumber == (key as ValueKey).value)
+              .first;
+        } else {
+          state = context.read<CreditCardBloc>().state;
+        }
+        return GestureDetector(
+            onTap: () {
+              context.read<CreditCardBloc>().add(ToggleCardView());
+            },
+            child: Stack(children: <Widget>[
+              AnimatedPositioned(
+                  curve: Curves.elasticOut,
+                  duration: const Duration(milliseconds: 2800),
+                  child: AnimatedContainer(
+                      alignment: Alignment.center,
+                      duration: const Duration(milliseconds: 300),
+                      child: state.showBackView
+                          ? _buildBackView(cardModel: state, context: context)
+                          : _buildFrontView(
+                              cardModel: state, context: context)))
+            ]));
+      },
+    );
   }
 
   Widget _buildFrontView(
